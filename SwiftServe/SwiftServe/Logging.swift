a1ab82a5ddd211f1a7e2e646afd0f7ae3090c7da
Anthony Picciano <anthony.picciano@gmail.com>
2014-07-22 16:38:23 -0600

    clean up Logging class

--- SwiftServe/SwiftServe/Logging.swift
+++ SwiftServe/SwiftServe/Logging.swift
@@ -24,22 +24,16 @@ class Logging:Filter
     
     func logEntry(connection: Connection) -> String
     {
-        let host = connection.socket.connectedHost
-        let ident = "-" //TODO: Implement Ident
-        let user = "-" //TODO: Get Remote-User
-        
+        let host:String = connection.socket.connectedHost
+        let ident:String = "-" //TODO: Implement Ident
+        let user:String = "-" //TODO: Get Remote-User
         let df = NSDateFormatter()
         df.dateFormat = dateFormatString
-        let dateString = df.stringFromDate(NSDate())
+        let dateString:String = df.stringFromDate(NSDate())
+        let requestDescription:String = connection.request!.description
+        let responseDescription:String = connection.response!.description
         
-        // line below leaks memory. swift bug?
-        // let log = "\(host) \(ident) \(user) [\(date)] \"\(connection.request!.description)\" \(connection.response!.description)"
-        
-        var log = host + " " + ident + " " + user
-        log += " [" + dateString + "]"
-        log += " \"" + connection.request!.description
-        log += "\" " + connection.response!.description
-        
+        let log = "\(host) \(ident) \(user) [\(dateString)] \"\(requestDescription)\" \(responseDescription)"
         return log
     }
 }
